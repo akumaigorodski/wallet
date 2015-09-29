8d7fc8f0f45ea6fee3a8b7f2a9ed9e0108d1fac4
Bitcoins wallet <admin@btcontract.com>
2015-09-29 20:07:06 +0300

    Provide creation date on wallet restore

--- app/src/main/java/com/btcontract/wallet/WalletRestoreActivity.scala
+++ app/src/main/java/com/btcontract/wallet/WalletRestoreActivity.scala
@@ -1,27 +1,47 @@
 package com.btcontract.wallet
 
 import org.bitcoinj.store.SPVBlockStore
+import java.util.{Date, Calendar}
 import android.os.Bundle
 import android.view.View
+import Utils.wrap
 
-import android.widget.{Button, EditText, TextView, LinearLayout}
 import org.bitcoinj.wallet.{KeyChainGroup, DeterministicSeed}
 import org.bitcoinj.core.{PeerGroup, BlockChain, Wallet}
+import android.app.{DatePickerDialog, DialogFragment}
+import Calendar.{YEAR, MONTH, DATE}
+import android.widget._
 
 
-class WalletRestoreActivity extends TimerActivity {
+class WalletRestoreActivity extends TimerActivity { me =>
   lazy val restoreProgess = findViewById(R.id.restoreProgess).asInstanceOf[LinearLayout]
   lazy val restoreInfo = findViewById(R.id.restoreInfo).asInstanceOf[LinearLayout]
   lazy val restoreWallet = findViewById(R.id.restoreWallet).asInstanceOf[Button]
   lazy val restoreCode = findViewById(R.id.restoreCode).asInstanceOf[TextView]
+  lazy val restoreWhen = findViewById(R.id.restoreWhen).asInstanceOf[Button]
   lazy val password = findViewById(R.id.restorePass).asInstanceOf[EditText]
   lazy val spin = findViewById(R.id.restoreSpin).asInstanceOf[TextView]
+  val may1st2014 = 1398902400L * 1000
+  var when = Calendar.getInstance
+
+  class WhenPicker extends DialogFragment with android.app.DatePickerDialog.OnDateSetListener {
+    def onDateSet(dp: DatePicker, yr: Int, mn: Int, dt: Int) = wrap(updateWhen) _ apply when.set(yr, mn, dt)
+    def dp = new DatePickerDialog(me, this, when get YEAR, when get MONTH, when get DATE)
+
+    override def onCreateDialog(state: Bundle) = dp match { case dialog =>
+      dialog.getDatePicker setMinDate new Date(may1st2014).getTime
+      dialog.getDatePicker setMaxDate (new Date).getTime
+      dialog
+    }
+  }
 
   // Initialize this activity, method is run once
   override def onCreate(savedInstState: Bundle) =
   {
     super.onCreate(savedInstState)
     setContentView(R.layout.activity_restore)
+    when setTimeInMillis may1st2014
+    updateWhen
 
     val changeListener = new TextChangedWatcher {
       override def onTextChanged(s: CharSequence, x: Int, y: Int, z: Int) = {
@@ -47,14 +67,14 @@ class WalletRestoreActivity extends TimerActivity {
       startAsync
 
       def startUp = {
-        val may1st2014 = 1398902400L
-        val mnemonic = restoreCode.getText.toString
-        val ds = new DeterministicSeed(mnemonic, null, "", may1st2014)
+        val mnemo = restoreCode.getText.toString
+        val whenTime = when.getTimeInMillis / 1000
+        val ds = new DeterministicSeed(mnemo, null, "", whenTime)
         val keyChainGroup = new KeyChainGroup(app.params, ds)
         store = new SPVBlockStore(app.params, app.chainFile)
         wallet = new Wallet(app.params, keyChainGroup)
         app.kit encryptWallet password.getText
-        useCheckPoints(may1st2014)
+        useCheckPoints(whenTime)
 
         // Must be initialized after checkpoints
         blockChain = new BlockChain(app.params, wallet, store)
@@ -69,5 +89,7 @@ class WalletRestoreActivity extends TimerActivity {
     }
 
   def recWallet(recoverWalletView: View) = hideKeys(doRecoverWallet)
-  override def onBackPressed = Utils.wrap(super.onBackPressed)(app.kit.stopAsync)
+  def setWhen(v: View) = (new WhenPicker).show(getFragmentManager, "whenCreatedTime")
+  def updateWhen = restoreWhen setText String.format("%1$tB %1$te, %1$tY", when)
+  override def onBackPressed = wrap(super.onBackPressed)(app.kit.stopAsync)
 }
