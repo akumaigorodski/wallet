319d4f0a27ed99e9a59c627a706020d74572bcd5
anton <admin@btcontract.com>
2016-07-12 19:31:28 +0300

    Started adding Bitcoin scripts

--- app/src/main/java/com/btcontract/wallet/helper/Digests.java
+++ app/src/main/java/com/btcontract/wallet/helper/Digests.java
@@ -1,11 +1,13 @@
 package com.btcontract.wallet.helper;
 
+import org.spongycastle.crypto.digests.RIPEMD160Digest;
 import org.spongycastle.crypto.digests.SHA256Digest;
 import org.spongycastle.crypto.params.KeyParameter;
 import org.spongycastle.crypto.macs.HMac;
+import org.bitcoinj.core.Sha256Hash;
 
 
-public final class HMacSha256 {
+public final class Digests {
     static HMac createHmacSha256Digest(byte[] key) {
         SHA256Digest digest = new SHA256Digest();
         HMac hMac = new HMac(digest);
@@ -13,15 +15,23 @@ public final class HMacSha256 {
         return hMac;
     }
 
-    static byte[] hmacSha256(HMac hmacSha256, byte[] input) {
+    static byte[] hmacSha256(HMac hmacSha256, byte[] data) {
         hmacSha256.reset();
-        hmacSha256.update(input, 0, input.length);
+        hmacSha256.update(data, 0, data.length);
         byte[] out = new byte[32];
         hmacSha256.doFinal(out, 0);
         return out;
     }
 
-    public static byte[] make(byte[] key, byte[] data) {
+    public static byte[] hmacSha256(byte[] key, byte[] data) {
         return hmacSha256(createHmacSha256Digest(key), data);
+    }
+
+    public static byte[] ripemd160(byte[] input) {
+        RIPEMD160Digest digest = new RIPEMD160Digest();
+        digest.update(input, 0, input.length);
+        byte[] out = new byte[20];
+        digest.doFinal(out, 0);
+        return out;
     }
 }
--- app/src/main/java/com/btcontract/wallet/lightning/Scripts.scala
+++ app/src/main/java/com/btcontract/wallet/lightning/Scripts.scala
@@ -1,12 +1,39 @@
 package com.btcontract.wallet.lightning
 
 import org.bitcoinj.core._
-import org.bitcoinj.script.Script
+import org.bitcoinj.script.ScriptOpCodes._
+import org.bitcoinj.script.{ScriptBuilder, Script}
+import com.btcontract.wallet.helper.Digests.ripemd160
 import org.bitcoinj.script.Script.ALL_VERIFY_FLAGS
 import com.btcontract.wallet.Utils.Bytes
 
 
 object Scripts {
+  def scriptPubKeyHtlcSend(ourKey: ECKey, theirKey: ECKey, absTimeout: Long,
+                           relTimeout: Long, rHash: Bytes, commitRevoke: Bytes) =
+
+    new ScriptBuilder op OP_SIZE number 32 op OP_EQUALVERIFY /* check data size */ op
+      OP_HASH160 op OP_DUP data ripemd160(rHash) op OP_EQUAL /* is it an r-value? */ op
+      OP_SWAP data ripemd160(commitRevoke) op OP_EQUAL /* or is it a revocation preimage? */ op
+      OP_ADD /* if either r-value or revocation preimage match */ op OP_IF data theirKey.getPubKey op
+      OP_ELSE number absTimeout op OP_CHECKLOCKTIMEVERIFY number relTimeout op OP_NOP3 /* OP_CLTV */ op
+      OP_2DROP data ourKey.getPubKey op OP_ENDIF op OP_CHECKSIG
+
+  def scriptPubKeyHtlcReceive(ourKey: ECKey, theirKey: ECKey, absTimeout: Long,
+                              relTimeout: Long, rHash: Bytes, commitRevoke: Bytes) =
+
+    new ScriptBuilder op OP_SIZE number 32 op OP_EQUALVERIFY /* check data size */ op
+      OP_HASH160 op OP_DUP data ripemd160(rHash) op OP_EQUAL /* is it an r-value? */ op
+      /* if it's an r-value then I can spend this output in <relTimeout> number of blocks */
+      OP_IF number relTimeout op OP_NOP3 /* OP_CLTV */ op OP_2DROP data ourKey.getPubKey op
+      /* else they can spend output now if they know revocation or after <absTimeout> */
+      OP_ELSE data ripemd160(commitRevoke) op OP_EQUAL op OP_NOTIF number absTimeout op
+      OP_CHECKLOCKTIMEVERIFY op OP_DROP op OP_ENDIF op OP_CHECKSIG
+
+  def redeemSecretOrDelay(delayedKey: ECKey, relTimeout: Long, keyIfSecretKnown: ECKey, hashOfSecret: Bytes) =
+    new ScriptBuilder op OP_HASH160 data ripemd160(hashOfSecret) op OP_EQUAL op OP_IF data keyIfSecretKnown.getPubKey op
+      OP_ELSE number relTimeout op OP_NOP3 /* OP_CLTV */ op OP_DROP data delayedKey.getPubKey op OP_ENDIF op OP_CHECKSIG
+
   def makeAnchorTx(ourCommitPub: ECKey, theirCommitPub: ECKey, amount: Long): Transaction = ???
 
   def makeCommitTx(inputs: java.util.List[TransactionInput], ourFinalKey: Bytes, theirFinalKey: Bytes,
