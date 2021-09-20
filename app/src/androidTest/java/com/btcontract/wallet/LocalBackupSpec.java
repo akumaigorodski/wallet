6eea4d59b807e0944b0fac194dcc31f2f01000da
anton <akumaigorodski@gmail.com>
2021-09-20 11:04:29 +0300

    Remove rules test library

--- app/src/androidTest/java/com/btcontract/wallet/LocalBackupSpec.java
+++ app/src/androidTest/java/com/btcontract/wallet/LocalBackupSpec.java
@@ -1,13 +1,10 @@
 package com.btcontract.wallet;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.GrantPermissionRule;
-
 import com.btcontract.wallet.utils.LocalBackup;
 import com.google.common.io.Files;
 
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
