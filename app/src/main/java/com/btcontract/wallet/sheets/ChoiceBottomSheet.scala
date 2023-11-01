e58fc2780a469a19b0d6c21216f4d3d44c98b1ef
Anton Kumaigorodski <akumaigorodski@gmail.com>
2023-11-01 18:50:47 +0200

    UI nits

--- app/src/main/java/com/btcontract/wallet/sheets/ChoiceBottomSheet.scala
+++ app/src/main/java/com/btcontract/wallet/sheets/ChoiceBottomSheet.scala
@@ -8,16 +8,17 @@ import com.btcontract.wallet.utils.OnListItemClickListener
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
-class BaseChoiceBottomSheet(list: ListView) extends BottomSheetDialogFragment { me =>
+class ChoiceBottomSheet(list: ListView, tag: AnyRef, host: ChoiceReceiver) extends BottomSheetDialogFragment {
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, state: Bundle): View = list
-}
 
-class ChoiceBottomSheet(list: ListView, tag: AnyRef, host: ChoiceReceiver) extends BaseChoiceBottomSheet(list) { me =>
-  override def onViewCreated(view: View, state: Bundle): Unit =
+  override def onViewCreated(view: View, state: Bundle): Unit = {
+    view.setBackgroundResource(com.btcontract.wallet.R.color.chip_default_text_color)
+
     list setOnItemClickListener new OnListItemClickListener {
       def onItemClicked(itemPosition: Int): Unit = {
         host.onChoiceMade(tag, itemPosition)
         dismiss
       }
     }
+  }
 }
